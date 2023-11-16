ac40ed4b4e048cd48cc354b541b9edd904bf8872
Christian Alquisada <calquisada@aol.com>
2023-11-15 20:24:11 -0800

    commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import {useEffect, useState } from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Gallery from './components/Gallery'
 import SearchBar from './components/SearchBar'
+import AlbumView from './components/AlbumView'
+import ArtistView from './components/ArtistView'
 import { DataContext } from './context/DataContext'
 
 function App() {
@@ -33,11 +36,19 @@ function App() {
 
 	return (
 		<div>
-			<SearchBar handleSearch = {handleSearch}/>
-			{message}
-			<DataContext.Provider value={data}>
-				<Gallery />
-			</DataContext.Provider>
+    	{message}
+        	<Router>
+            	<Routes>
+                	<Route path="/" element={
+                    	<Fragment>
+                        	<SearchBar handleSearch = {handleSearch}/>
+                        	<Gallery data={data} />
+                    	</Fragment>
+                	} />
+                	<Route path="/album/:id" element={<AlbumView />} />
+                	<Route path="/artist/:id" element={<ArtistView />} />
+            	</Routes>
+        	</Router>
 		</div>
   	);
 }
